113ae2c744650aedfab5d5c76a8921e0b18dda77
Hiba243 <hibafatima24@gmail.com>
2021-05-23 19:42:32 +0530

    added portfolio data

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+//scene.add(torus);
 
 // Lights
 
@@ -36,13 +36,13 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+//scene.add(lightHelper, gridHelper)
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const material = new THREE.MeshStandardMaterial({ color: 0x8f6df5 });
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
@@ -80,3 +80,94 @@ function animate() {
 }
 
 animate();
+
+//hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__menu");
+const navLink = document.querySelectorAll(".nav__link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  var sidebar = document.getElementsByClassName("hamburger");
+  if (sidebar[0].classList.contains('active')) {
+    var bodyelem = document.getElementsByTagName('body');
+    bodyelem[0].style.overflow = 'hidden';
+  }
+  else {
+    var bodyelem = document.getElementsByTagName('body');
+    bodyelem[0].style.overflow = 'visible';
+  }
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  var bodyelem = document.getElementsByTagName('body');
+  bodyelem[0].style.position = 'relative';
+}
+
+//copy to clipboard
+const links = document.querySelectorAll('.copy-click');
+const cls = {
+  copied: 'is-copied',
+  hover: 'is-hovered'
+}
+
+const copyToClipboard = str => {
+  const el = document.createElement('input');
+  str.dataset.copyString ? el.value = str.dataset.copyString : el.value = str.text;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.opacity = 0;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
+
+const clickInteraction = (e) => {
+  e.preventDefault();
+  copyToClipboard(e.target);
+  e.target.classList.add(cls.copied);
+  setTimeout(() => e.target.classList.remove(cls.copied), 1000);
+  setTimeout(() => e.target.classList.remove(cls.hover), 700);
+}
+
+Array.from(links).forEach(link => {
+  link.addEventListener('click', e => clickInteraction(e));
+  link.addEventListener('keypress', e => {
+    if (e.keyCode === 13) clickInteraction(e);
+  });
+  link.addEventListener('mouseover', e => e.target.classList.add(cls.hover));
+  link.addEventListener('mouseleave', e => {
+    if (!e.target.classList.contains(cls.copied)) {
+      e.target.classList.remove(cls.hover);
+    }
+  });
+});
+
+//carousel
+let carousel = document.getElementById("carousel"),
+  slides = carousel.getElementsByTagName("li"),
+  counter = 0,
+  liList = Array.prototype.slice.call(slides);
+
+setInterval(function () {
+  slides[counter].style.opacity = 0; // Hide the previous image
+  counter = (counter + 1) % slides.length; // Increment counter
+  slides[counter].style.opacity = 1; // Show the next image
+}, 3000); // setInterval
+
+//cursor
+
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
